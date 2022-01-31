8f8cbeed8be487141331d8b3e3bed8a4fe5e07c1
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-01-31 14:28:15 -0300

    Contador

--- aprendendojs/DIO JS/htmlcssjs/js/script.js
+++ aprendendojs/DIO JS/htmlcssjs/js/script.js
@@ -1,13 +1,22 @@
-var currentNumberWrapper = document.getElementById("currentNumber")
-var currentNumber = 0;
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0
 
-function aumenta(){
-  currentNumber = currentNumber + 1;
-  currentNumberWrapper.innerHTML = currentNumber;
+function aumenta() {
+  currentNumber = currentNumber + 1
+  currentNumberWrapper.innerHTML = currentNumber
 }
 
-
-function diminui(){
-  currentNumber = currentNumber - 1;
-  currentNumberWrapper.innerHTML = currentNumber;
+function diminui() {
+  currentNumber = currentNumber - 1
+  currentNumberWrapper.innerHTML = currentNumber
 }
+
+/*function vermelho() {
+  if (currentNumber <= -1) {
+    document.getElementById('currentNumber').style.color = 'red'
+  }
+}*/
+
+/*addEventListener()
+var currentNumber = document.getElementById("currentNumber");
+  currentNumber.addEventListener("click", currentNumber < 0);*/
