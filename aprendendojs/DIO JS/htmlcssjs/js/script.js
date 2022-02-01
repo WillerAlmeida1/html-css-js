e2739af7e3c4bd0e0d3dd78a694a103603d1deea
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-02-01 14:52:47 -0300

    Lista de afazeres e concluindo desafio de contador

--- aprendendojs/DIO JS/htmlcssjs/js/script.js
+++ aprendendojs/DIO JS/htmlcssjs/js/script.js
@@ -4,13 +4,22 @@ var currentNumber = 0
 function aumenta() {
   currentNumber = currentNumber + 1
   currentNumberWrapper.innerHTML = currentNumber
+
+  if (currentNumber >= 1) {
+    document.getElementById('currentNumber').style.color = 'green'
+  }
 }
 
 function diminui() {
   currentNumber = currentNumber - 1
   currentNumberWrapper.innerHTML = currentNumber
+
+  if (currentNumber <= -1) {
+    document.getElementById('currentNumber').style.color = 'red'
+  }
 }
 
+//Vc tava até fazendo oq era pra fazer mas fora da função onde deveria estar kskskks, mas boa :D
 /*function vermelho() {
   if (currentNumber <= -1) {
     document.getElementById('currentNumber').style.color = 'red'
